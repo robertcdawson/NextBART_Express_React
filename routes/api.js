529d323eeae575f22cc50e916d247a7d2bc512e1
Robert Dawson <manatees@gmail.com>
2015-11-30 21:56:33 -0800

    Create route for station selection

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var request = require('request');
-
 var router = express.Router();
 
 var apiKey = 'J5HV-5EUE-5ZZ9-XERY';
-var station = 'NCON';
-
-var apiUrl = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+station+'&key='+apiKey;
 
 /* GET BART API */
-router.get('/', function(req, res, next) {
+// ? means optional
+router.get('/:station?', function(req, res, next) {
+  // If no param is given, default to NCON (North Concord/Martinez) station
+  var station = (req.params.station) ? req.params.station : 'NCON';
+
+  var apiUrl = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig='+station+'&key='+apiKey;
+
   request(apiUrl, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(response.body);
     }
   });
+});
+
+router.get('/station/:id', function(req, res) {
+    res.send(req.params.id);
 });
 
 module.exports = router;
